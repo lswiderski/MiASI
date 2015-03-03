2512d7d99c9765244314264052a75426f2b0505f
Neufrin <neufrin@gmail.com>
2015-03-03 13:31:53 +0100

    Aplikacja generuje poprawne dane

---
+++ zad2/zad2/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    public class Data
+    {
+        public int Lambda { get; set; }
+        public double P { get; set; }
+
+        public Data(int lambda, double p)
+        {
+            Lambda = lambda;
+            P = p;
+        }
+    }
+}
--- zad2/zad2/MainWindow.xaml.cs
+++ zad2/zad2/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace zad2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Data> datas;
         public MainWindow()
         {
+            datas = new List<Data>();
             InitializeComponent();
+            fillData();
+            DataGrid1.ItemsSource = datas;
         }
+
+        public void fillData()
+        {
+            for (int i = 2; i <=16; i+=2)
+            {
+                datas.Add(new Data(i,calcP(i,16)));
+            }
+        }
+        public double calcP(int lambda, int kmax)
+        {
+            double p=0;
+            for (int k = 0; k <= kmax; k++)
+            {
+                var x = (Math.Pow(lambda, k)/Factorial(k))*Math.Pow(2.7183, (-1)*lambda);
+                p += x;
+            }
+            return p;
+        }
+
+        long Factorial(long i)
+        {
+            if (i <= 1)
+                return 1;
+            return i * Factorial(i - 1);
+        }
+        
     }
 }
