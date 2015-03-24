4772e380921a708a3daee2bc5d923ebee1d882e0
Neufrin <neufrin@gmail.com>
2015-03-24 15:33:27 +0100

    zad 5: sprawozdanie

--- zad5/zad5/Data.cs
+++ zad5/zad5/Data.cs
@@ -48,7 +48,7 @@ namespace zad5
                 });
 
             }
-            for (int i = 0; i < N; i++)
+            for (int i = 1; i <= N; i++)
             {
                 Stany.Add(new Pi()
                 {
